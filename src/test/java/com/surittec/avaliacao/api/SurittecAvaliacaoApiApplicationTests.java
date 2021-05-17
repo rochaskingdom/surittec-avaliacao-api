52407fb948d18db101d8afd9cc3b9563ecea2b40
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-05-17 17:09:08 -0300

    Projeto iniciado

---
+++ src/test/java/com/surittec/avaliacao/api/SurittecAvaliacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surittec.avaliacao.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurittecAvaliacaoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
